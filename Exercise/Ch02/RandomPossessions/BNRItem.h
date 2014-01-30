0beb53a62deb8ee5e54c354ccdacd83cc169347f
LartAceIgnacio <l.ignacio@klab.com>
2014-01-30 16:45:30 +0800

    With Chapter 02

---
+++ Exercise/Ch02/RandomPossessions/BNRItem.h
@@ -0,0 +1,35 @@
+//
+//  BNRItem.h
+//  RandomPossessions
+//
+//  Created by Ace Ignacio on 1/30/14.
+//  Copyright (c) 2014 Ace Ignacio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *itemName;
+    NSString *serialNumber;
+    int valueInDollars;
+    NSDate *dateCreated;
+}
+
+@property NSString *itemName;
+@property NSString *serialNumber;
+@property int valueInDollars;
+@property NSDate *dateCreated;
+
+-(id)initWithName:(NSString *)name
+     serialNumber:(NSString *)sNumber
+   valueInDollars:(int)value;
+
+-(id)initWithName:(NSString *)name;
+
+-(id)initWithNAme:(NSString *)name
+     serialNumbeR:(NSString *)sNumber;
+
++(id)randomItem;
+
+@end
